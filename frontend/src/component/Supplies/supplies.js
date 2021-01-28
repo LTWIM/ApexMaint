318161155352267ba6c1cf404999e40b68f9d507
paulkwchoi92 <paulkwchoi92@gmail.com>
2021-01-28 07:34:45 -0800

    stuff

--- frontend/src/component/Supplies/supplies.js
+++ frontend/src/component/Supplies/supplies.js
@@ -2,14 +2,30 @@ import React, { Component } from 'react'
 import {BrowserRouter, Route, Link} from 'react-router-dom'
 import LongBanner from '../module/long_banner/Longbanner'
 import "./styles.scss"
+const FakeData = [
+  "Restroom",
+  "Restroom",
+  "Restroom",
+  "Restroom",
+  "Restroom",
+  "Restroom"
+]
 const Supplies = () => {
   return (
     <div className="supplies-wrapper">
       <LongBanner type='type1' backgroundImageUrl="/img/bottomLogo.svg" displayButton={false} content={<div><span className="big-sp">SUPPLIES</span>
           <br/>
           <span className="small-sp">Supplies at wholesale price delivered for free</span></div> }/>
-      <div className="supplies-hdr-container"></div>
-      <div className="supplies-cntt-cntr" style={{backgroundImage: `url(/img/restroom.png)`}}></div>
+      <div className="supplies-hdr-container">
+        <span className="supplies-hdr">Supplies</span>
+      </div>
+      <div className="supplies-cntt-cntr" style={{ backgroundImage: `url(/img/restroom.png)` }}>
+        {FakeData.map((ele, idx) => {
+          return <div className="supply-squares">
+            <span className='supply-square-spn'>{ele}</span>
+          </div>
+        })}
+      </div>
       <div className='wall-paper-bottom spplies-btm'>
           <div className='darkbox'>
             <LongBanner type='type4' displayButton/>   
