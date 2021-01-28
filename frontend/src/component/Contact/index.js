84de1946c6f61b89fc44e86977885515d1a8ccd6
paulkwchoi92 <paulkwchoi92@gmail.com>
2021-01-28 06:39:57 -0800

    contact us finished

--- frontend/src/component/Contact/index.js
+++ frontend/src/component/Contact/index.js
@@ -8,17 +8,31 @@ const Contact = () => {
     <div className='contact-wrapper'>
       <LongBanner type='type4' backgroundImageUrl="/img/bottomLogo.svg" displayButton={false}/>
       <div className="contact-cntt-cnt">
-        <div className='contct-cnnt-hdr'></div>
-        <div className='contct-inpt-cnt'>
-          <input type='text' className="contct-inpt"/>
+        <div className='contct-cnnt-hdr'>
+          <h2>Customer Services</h2>
+          <span>We'll get back to you within 1 business day from your inquiry.<br />
+          In order for us to provide you with the bes fitted service, please describe your<br />
+          inquiry as detailed as you can in the details area.
+          </span>
         </div>
         <div className='contct-inpt-cnt'>
-          <input type='text' className="contct-inpt"/>
+          <input type='text' className="contct-inpt" placeholder="Name"/>
         </div>
         <div className='contct-inpt-cnt'>
-          <input type='text' className="contct-inpt"/>
+          <input type='text' className="contct-inpt" placeholder="Email"/>
         </div>
-        <div classname='contct-txtar-cnt'></div>
+        <div className='contct-inpt-cnt'>
+          <input type='text' className="contct-inpt" placeholder="Phone"/>
+        </div>
+         <div className='contct-inpt-cnt'>
+          <input type='text' className="contct-inpt" placeholder="Subject"/>
+        </div>
+        <div className='contct-txtar-cnt'>
+          <textarea type='text' className='contct-txtar' placeholder="Details..."></textarea>
+        </div>
+        <div className="contct-sbmit-btn-cnt">
+          <button className='contct-sbmit-btn'>Send</button>
+        </div>
       </div>
     </div>
 
