fc4d4c20ca9e3d15cba058b8bb36c78adbd1d97f
paulkwchoi92 <paulkwchoi92@gmail.com>
2021-01-28 01:21:40 -0800

    added nodamiler route

--- routes/client.js
+++ routes/client.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Client = require("../models/Client")
+const nodeMailer = require('nodemailer')
 router.get("/get_all_clients", async (req, res) => {
   try {
     let allClients = await Client.find({})
@@ -10,5 +11,31 @@ router.get("/get_all_clients", async (req, res) => {
     res.status(500).send(err)
   }
 })
+
+
+router.post("/create_client", async (req, res) => { 
+  
+  try {
+    let newClient = await new Client({
+
+    })
+
+    res.status(200).json({ newClient })
+  } catch (err) {
+    console.log(err)
+    res.status(500).send(err)
+  }
+
+})
+
+router.post("/delete_client", async(req, res) => {
+  try {
+
+  } catch(err) {
+    console.log(err)
+    res.status(500).send(err)
+  }
+})
+
 
 module.exports = router;
