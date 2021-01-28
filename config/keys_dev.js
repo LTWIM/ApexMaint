a6a7237e994f2b11867ca9dac83f0ae37e4365cf
Joy <joyl109@yahoo.com>
2021-01-27 17:04:37 -0800

    revmoved

--- config/keys_dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongoURI: "mongodb+srv://apexAdmin:pOZyy0omdC9Jwkxe@apexdatacluster.i6dv2.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "zzB?;6ywhS@Z3uq",
-  awsAccessKey: "AKIASDFBVBUGIYZUW2PJ",
-  awsSecretKey: "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",
-  awsRegion: "us-east-2",
-};
