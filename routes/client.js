1adcb5b74bff8c9131349b3686bc174545bbe1b7
paulkwchoi92 <paulkwchoi92@gmail.com>
2021-01-28 01:14:20 -0800

    stuff

--- routes/client.js
+++ routes/client.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const router = express.Router();
-
-
+const Client = require("../models/Client")
+router.get("/get_all_clients", async (req, res) => {
+  try {
+    let allClients = await Client.find({})
+    res.status(200).json({allClients})
+  } catch (err) {
+    console.log(err)
+    res.status(500).send(err)
+  }
+})
 
 module.exports = router;
