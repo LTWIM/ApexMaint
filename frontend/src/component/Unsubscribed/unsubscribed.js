ca6cabc935d1d383bb6cb21d3a88267497955473
Joy <joyl109@yahoo.com>
2021-03-01 20:54:08 -0800

    typo fixed

--- frontend/src/component/Unsubscribed/unsubscribed.js
+++ frontend/src/component/Unsubscribed/unsubscribed.js
@@ -10,7 +10,7 @@ export class unsubscribed extends React.Component{
               <img src="/logo/apexOld.png" width='30%'></img>
             </div>
             <div className="unsubscribe-innerbox-top-textbox">
-              You are now successfully unsbscribed
+              You are now successfully unsubscribed
             </div>
             <div className="unsubscribe-text">
               You've been successfully unsubscribed from out newsletters and your email has been removed from the list. We're really sorry to see you go.
