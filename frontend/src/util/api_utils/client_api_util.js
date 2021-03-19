9cefc7ab1bc1cda5b4ee75b5dbd9fae8a52f2796
manha1895 <manha1895@hotmail.com>
2021-03-19 01:09:30 -0700

    test

---
+++ frontend/src/util/api_utils/client_api_util.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const getClientDatas =  () => {
+    return axios.get('/api/clients')
+}
+
+export const addClientData = (data) => {
+    return axios.post('/api/clients', data)
+}
+
+export const deleteClientData = (id) => {
+    return axios.delete(`/api/clients/${id}`)
+}
+
+export const editClientData = (id, data) => {
+    return axios.patch(`/api/clients/${id}`, data)
+}
