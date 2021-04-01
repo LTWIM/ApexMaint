2f7f896624072b3217ce0e4898aaa5db93c64774
manha1895 <manha1895@hotmail.com>
2021-04-01 10:46:34 -0700

    *

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(
   bodyParser.json({ limit: "50mb", extended: false, parameterLimit: 100000 })
 );
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.use(express.static("frontend/build"));
   app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
